f53b94df734b6b2fba070fde89be8083aead05df
AniT-A <tina_savolainen@outlook.com>
2024-09-30 20:55:57 +0300

    Added Robots file and improved alignment

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,11 @@
-/*document.addEventListener('DOMContentLoaded', () => {
-    fetchData('', '');
-});*/
-    function fetchData(dataFile, imageUrl) {
-        fetch(dataFile + '.txt')
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById('text-container').innerHTML = data;
-                const imageElement = document.getElementById('image');
-                imageElement.src = imageUrl;
-                imageElement.style.display = 'block';
-            })
-            .catch(error => document.getElementById('text-container').innerText = 'Error: ' + error.message);
-    }
-
+function fetchData(dataFile, imageUrl) {
+    fetch(dataFile + '.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('text-container').innerHTML = data;
+            const imageElement = document.getElementById('image');
+            imageElement.src = imageUrl;
+            imageElement.style.display = 'block';
+        })
+        .catch(error => document.getElementById('text-container').innerText = 'Error: ' + error.message);
+}
